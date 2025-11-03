fa6a3c48bcd62225a19574a136e6db1327dea5ec
emilmadry <redvarg91@gmail.com>
2025-11-03 19:11:33 +0100

    test: dotenv added .env added to git ignore

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title', { tag: '@smoke' }, async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -17,3 +17,4 @@ test('get started link', { tag: '@regression' },  async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
